60034857e27a8d3195e45e74611f3e9a7d4baa14
Suyog Jadhav <jsuyog2@gmail.com>
2023-10-23 16:11:25 +0530

    add UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+declare var $: any;
+declare var bootstrap: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'WeddingInvitation';
+  title = 'Suyog Weds Payal';
+  ngOnInit() {
+    $('#home .tag').addClass('visible');
+    $(".smooth").on("scroll", function () {
+      var pageTop = $(document).scrollTop();
+      var pageBottom = pageTop + $(window).height();
+      var tags = $("section");
+      for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+        if ($(tag).position().top < pageBottom) {
+          $(tag).children('.tag').addClass("visible");
+        } else {
+          $(tag).children('.tag').removeClass("visible");
+        }
+      }
+    });
+
+  }
 }
