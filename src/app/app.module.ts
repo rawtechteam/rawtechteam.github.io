35c1d64b99852f46d050b47d6ee9311b08868770
Suyog Jadhav <jsuyog2@gmail.com>
2023-10-30 01:36:04 +0530

    add analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { LazyLoadImageModule } from 'ng-lazyload-image';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './layout/home/home.component';
+import { NgxGoogleAnalyticsModule } from 'ngx-google-analytics';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomeComponent } from './layout/home/home.component';
   imports: [
     BrowserModule,
     LazyLoadImageModule,
+    NgxGoogleAnalyticsModule.forRoot('G-XTH7TN48XV'),
     AppRoutingModule
   ],
   providers: [Title],
